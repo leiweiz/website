8caa7cb49472276c3b107b8f1129d26b2def3de5
leiweiz <zhengleiwei@hotmail.com>
2016-06-11 16:22:49 -0400

    foods: request all photos

--- client/controllers/hungry/components/foods/foodsController.js
+++ client/controllers/hungry/components/foods/foodsController.js
@@ -3,7 +3,17 @@
  */
 'use strict';
 
-app.controller('FoodsController', ['$scope', '$routeParams',
-    function ($scope, $routeParams) {
+app.controller('FoodsController', ['$scope', '$routeParams', '$rootScope', '$http',
+    function ($scope, $routeParams, $rootScope, $http) {
+        $scope.loginUser = $rootScope.loginUser;
+        updatePhotos();
 
+        function updatePhotos() {
+            $http.get('/photos/list').then(function(res) {
+                if (res.status === 200){
+                    return $scope.photos = res.data;
+                }
+                console.log('fails', res.status);
+            });
+        }
     }]);
