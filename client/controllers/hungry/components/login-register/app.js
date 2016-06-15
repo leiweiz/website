41a10d88b8794ed45070d256c504a50b9f2a2ae3
leiweiz <zhengleiwei@hotmail.com>
2016-06-14 23:22:03 -0400

    login-register page: add photo detail

--- client/controllers/hungry/components/login-register/app.js
+++ client/controllers/hungry/components/login-register/app.js
@@ -87,6 +87,33 @@ app.controller('LoginRegisterController',
             });
         };
 
+        $scope.showPhotoDetail = function(ev, photo) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: PhotoDetailDialogController,
+                templateUrl: '/client/controllers/hungry/components/login-register/PhotoDetailDialogTemplate.ejs',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: useFullScreen,
+                locals : {
+                    photo: photo
+                }
+            })
+                .then(function(msg) {
+                    $scope.status = 'succeed';
+                    $scope.showSimpleToast('added new comment');
+                }, function(msg) {
+                    $scope.status = 'You cancelled the dialog.';
+                    $scope.showSimpleToast('cancelled');
+                });
+            $scope.$watch(function() {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function(wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
+
         $scope.showSimpleToast = function(message) {
             var pinTo = "top right";
             $mdToast.show(
@@ -188,5 +215,34 @@ function RegisterDialogController($scope, $mdDialog, $resource) {
         }
         // address ?
         return true;
+    }
+}
+
+function PhotoDetailDialogController($scope, $mdDialog, $http, photo) {
+    $scope.photo = photo;
+    $scope.comments = {};
+    $scope.userComment = '';
+    var url = '/commentsOfPhoto/' + $scope.photo._id;
+
+    updateComments();
+
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+
+    function updateComments(){
+        $http({
+            method: 'GET',
+            url: url
+        }).then(function successCallback(response) {
+            console.log('succeed');
+            $scope.comments = response.data;
+        }, function errorCallback(response) {
+            console.log('error');
+            console.log(response);
+        });
     }
 }
