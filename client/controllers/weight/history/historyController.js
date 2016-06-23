e19cb387de7d2418920d3d5f6dbe6a591d49e2d4
eyolei <zhengleiwei@gmail.com>
2016-06-23 09:55:14 -0400

    history: updated date output format

--- client/controllers/weight/history/historyController.js
+++ client/controllers/weight/history/historyController.js
@@ -12,4 +12,13 @@ app.controller('HistoryController',
             $scope.weightData = weightDataService.weightData;
         });
 
+        $scope.weekday = new Array(7);
+        $scope.weekday[0]=  "Sun";
+        $scope.weekday[1] = "Mon";
+        $scope.weekday[2] = "Tue";
+        $scope.weekday[3] = "Wed";
+        $scope.weekday[4] = "Thu";
+        $scope.weekday[5] = "Fri";
+        $scope.weekday[6] = "Sat";
+
     }]);
