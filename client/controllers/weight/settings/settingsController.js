884304f06349d4664475e68983282b5220ebd687
leiweiz <zhengleiwei@hotmail.com>
2016-06-23 23:22:55 -0400

    settingsTemplate: finish editting goal weight

--- client/controllers/weight/settings/settingsController.js
+++ client/controllers/weight/settings/settingsController.js
@@ -3,7 +3,45 @@
  */
 
 app.controller('SettingsController',
-    ['$scope', function ($scope) {
+    ['$scope', 'weightDataService', '$mdBottomSheet', '$mdToast',
+        function ($scope, weightDataService, $mdBottomSheet, $mdToast) {
 
+            $scope.goalWeight = weightDataService.goalWeight;
+            $scope.showEditGoalWeightBottomSheet = showEditGoalWeightBottomSheet;
 
-    }]);
+            function showEditGoalWeightBottomSheet() {
+                $mdBottomSheet.show({
+                    templateUrl: '/client/controllers/weight/settings/editGoalWeightBottomSheet.ejs',
+                    controller: 'EditGoalWeightBottomSheetCtrl',
+                    clickOutsideToClose: true
+                }).then(function(hideMsg) {
+                    $scope.goalWeight = weightDataService.goalWeight;
+                    $scope.showSimpleToast(hideMsg);
+                }, function(cancelMsg) {
+
+                });
+            }
+
+            $scope.showSimpleToast = function(message) {
+                var pinTo = "top right";
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent(message)
+                        .position(pinTo )
+                        .hideDelay(600)
+                );
+            };
+
+        }]);
+
+app.controller('EditGoalWeightBottomSheetCtrl',
+    ['$scope', '$mdBottomSheet', 'weightDataService',
+        function($scope, $mdBottomSheet, weightDataService) {
+            $scope.goalWeight = weightDataService.goalWeight;
+
+            $scope.editGoalWeight = function() {
+                // TODO: validate
+                weightDataService.goalWeight = $scope.goalWeight;
+                $mdBottomSheet.hide("succeed");
+            }
+        }]);
